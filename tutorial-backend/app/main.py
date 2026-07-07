48e6f841fcbbb4b4d159b77236c438cb201ceefd
sukkiti <phoenix20040404@gmail.com>
2026-07-08 02:15:05 +0900

    チュートリアルまでほとんど完成，fastapiで動作確認済み．残りはフロントエンドとの変数の受け渡しを調整する

--- tutorial-backend/app/main.py
+++ tutorial-backend/app/main.py
@@ -1,20 +1,14 @@
 from contextlib import asynccontextmanager
+from app import SUPABASE_URL, SUPABASE_KEY
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
-#別ファイル関数
-from database import init_database
-from routers.auth import sign_up_user, sign_in_user, sign_out_user
-
+from app.routers import gatya, auth
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # ---- サーバー起動時の処理 ----
     print("サーバー起動とDB初期化")
-    print("未実装")
-    init_database()
-    
     yield  # ここでサーバーが起動し、リクエストを待ち受けます
+    
     
     # ---- サーバー終了時の処理（あればここに書く） ----
     print("[LOG] サーバーを停止します")
@@ -37,18 +31,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/")
-def read_root():
-    return {"message": "FastAPI Server is running!"}
-
-@app.post("/login")
-def handle_login(data: dict):
-    print(f"[LOG] ログインリクエストを受信: {data.get('email')}")
-    return sign_in_user(data.get("email"), data.get("password"))
+app.include_router(gatya.router)
+app.include_router(auth.router)
 
 
-# main.py の一番最後に追加
+
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
